8e45c13c6c5dbeb124285e54552ab1d391d0eb62
Agnaldo Caldeira <agnaldoluizd@gmail.com>
2019-10-07 22:05:39 -0300

    Projeto iniciado mostrando a msg ola mundo

---
+++ projetoteste/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace projetoteste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola Mundo!");
+
+            Console.ReadLine();
+        }
+    }
+}
